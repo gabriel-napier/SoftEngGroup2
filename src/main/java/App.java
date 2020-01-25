f50fe13a58984db04859462cdf423391ac6b4a61
djblazkowicz <40417349@live.napier.ac.uk>
2020-01-25 17:13:12 +0000

    trying to get app.java to do load jdbc driver and connect to db

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,62 @@
 package SoftEngAPP;
 
+import java.sql.*;
+
 public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("Boo yah!");
+        try
+        {
+            // Load Database driver
+            Class.forName("SoftEngAPP.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
     }
 }
