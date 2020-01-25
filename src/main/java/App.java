a95b2f170a96a8e61df72bd37197ff72d6e835da
djblazkowicz <40417349@live.napier.ac.uk>
2020-01-25 17:55:11 +0000

    can't connect to database via jdbc...

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App
         try
         {
             // Load Database driver
-            Class.forName("SoftEngAPP.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
         }
         catch (ClassNotFoundException e)
         {
@@ -28,7 +28,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://softenggroup2_feature_db_1:3306/world", "root", "example");
                 System.out.println("Successfully connected");
                 // Wait a bit
                 Thread.sleep(10000);
