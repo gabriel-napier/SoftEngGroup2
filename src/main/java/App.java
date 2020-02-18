9124d4c8b618220b4d0bcbb1e547e0cfd9bd46b8
djblazkowicz <40417349@live.napier.ac.uk>
2020-02-18 19:31:52 +0000

    rewrote a bunch of code from main as functions and added more comments

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,18 @@
 package SoftEngAPP;
 
-import com.mysql.cj.PreparedQuery;
-import com.mysql.cj.x.protobuf.MysqlxPrepare;
+/**
+ *
+ * Software Engineering Methods Assessment
+ * Edinburgh Napier University 2020
+ * Graduate Apprenticeship SofDev class GROUP 2
+ *
+ * Callum Lackie
+ * Fraser MacLean
+ * Gabor Nemeth
+ * Gabriel Pinchuck
+ *
+ */
+
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,7 +20,6 @@ import java.sql.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * more comments!
@@ -18,6 +28,10 @@ public class App
 {
 
     public static void main(String[] args) throws IOException, SQLException {
+
+        /**
+         * loading JDBC DRIVER
+         */
         try
         {
             // Load Database driver
@@ -30,7 +44,9 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
+        /**
+         * Connection to the database, throw relevant exception in case of no success
+         */
         Connection con = null;
         int retries = 100;
         for (int i = 0; i < retries; ++i)
@@ -62,34 +78,27 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+        /**
+         * END OF DATABASE CONNECTION CODE
+         */
 
-        List<String> querynames = new ArrayList<String>();
-        List<SQLquery> queries = new ArrayList<SQLquery>();
-        File curDir = new File(".");
-        String[] fileNames = curDir.list();
-        for (int i = 0; i <= fileNames.length - 1; i++) {
-            System.out.println(fileNames[i]);
-            if (fileNames[i].contains(".sql")) {
-                querynames.add(fileNames[i]);
-            }
+        /**
+         * this list is used to store SQL queries
+         */
+        List<SQLquery> queries = updateQueryList();
 
-        }
-
-        for ( String querypath : querynames ) {
-
-            String content = new String(Files.readAllBytes(Paths.get(querypath)));
-            SQLquery newquery = new SQLquery(querypath,content);
-            queries.add(newquery);
-        }
-
+        /**
+         * this piece of code is used to run all available SQL queries
+         */
         for (SQLquery query : queries) {
             System.out.println(query.name);
             System.out.println(query.sql);
-
             System.out.println(getQueryResult(query, con));
         }
 
-
+        /**
+         * at the end of main, close database connection
+         */
         if (con != null)
         {
             try
@@ -105,25 +114,50 @@ public class App
 
     }
 
+    /**
+     * getQueryResult runs the selected SQL query on the database
+     * @param query is an SQLquery object
+     * @param con is the database connection
+     * @return output is the queries result in CSV format
+     * @throws SQLException
+     */
     public static String getQueryResult(SQLquery query, Connection con) throws SQLException {
+
+        //load selected database query and run it against database
+
         Statement stmt = con.createStatement();
         ResultSet result = stmt.executeQuery(query.sql);
         String output = "";
+
+        /**
+        * build output string
+        */
         while (result.next()) {
             ResultSetMetaData rsmd = result.getMetaData();
             int columnsnumber = rsmd.getColumnCount();
             for (int i = 1; i < columnsnumber + 1; i++) {
                 String name = rsmd.getColumnName(i);
                 if (i == columnsnumber) {
+
+                    /**
+                     * add new line when last column is reached otherwise just add "," for CSV format
+                     */
                     output = output + result.getString(name) + "\n";
                 } else {
                     output = output + result.getString(name) + ",";
                 }
             }
         }
+        /**
+         * return output string
+         */
         return output;
     }
 
+    /**
+     * SQLquery class
+     * used to create SQL query objects that can later be referred to for listing queries and running the desired database query by user
+     */
     public static class SQLquery {
 
         public String name;
@@ -134,7 +168,50 @@ public class App
             this.sql = newsql;
         }
 
+    }
+
+    /**
+     * updateQueryList loads the available SQL query strings from .xyz files in the runtime folder into a List so that they may be called on demand
+     */
+    public static List<SQLquery> updateQueryList() throws IOException {
+
+        List<String> querynames = new ArrayList<String>();
+        List<SQLquery> queries = new ArrayList<SQLquery>();
+        File curDir = new File(".");
+        String[] fileNames = curDir.list();
+
+        for (int i = 0; i <= fileNames.length - 1; i++) {
+
+            /**
+             * SELECT ONLY .XYZ FILES FROM FOLDER
+             */
+            System.out.println(fileNames[i]);
+            if (fileNames[i].contains(".xyz")) {
+
+                querynames.add(fileNames[i]);
+
+            }
+
+        }
+
+        /**
+         * create SQLquery objects with appropriate name and query string
+         */
+        for ( String querypath : querynames ) {
+
+            String content = new String(Files.readAllBytes(Paths.get(querypath)));
+            SQLquery newquery = new SQLquery(querypath.replace(".xyz",""),content);
+            queries.add(newquery);
+
+        }
+
+        /**
+         * return query list
+         */
+        return queries;
 
     }
+
+
 
 }
