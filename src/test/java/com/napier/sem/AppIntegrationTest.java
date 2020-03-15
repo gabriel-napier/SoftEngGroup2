be7c17ff32bb50a0ae45e7ac7a3b19f3b097236b
Fraser83 <fmaclean@gmail.com>
2020-03-15 10:32:29 +0000

    Added travis job staging for integration

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+}
